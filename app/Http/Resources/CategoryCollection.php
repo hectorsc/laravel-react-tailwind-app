110600997792bc5fd2f2dfce6f967ac230bf46f3
hectorsc <makarron90@gmail.com>
2021-01-05 13:49:59 +0100

    Category resources

---
+++ app/Http/Resources/CategoryCollection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CategoryCollection extends ResourceCollection
+{
+    public $collects = CategoryResource::class;
+   
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection
+        ];
+    }
+}

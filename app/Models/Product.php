14e03e505fed29fdc86ba037252e204fe4bcda84
hectorsc <makarron90@gmail.com>
2021-01-08 01:54:37 +0100

    BBDD:: añadida relaciones category y user con products y product con category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,19 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Product extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
 
-    public function user()
+    public function category()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(Category::class);
     }
 
-    public function products()
-    {
-        return $this->hasMany(Product::class);
-    }
 }

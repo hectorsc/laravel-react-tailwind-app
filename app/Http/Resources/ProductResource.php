3273fecd1ac84cc87de69ef3723033041b77ffc0
hectorsc <makarron90@gmail.com>
2021-01-08 01:57:40 +0100

    Product :: resources

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (int) $this->id,
+            'name' => (string) $this->name,
+            'ref' => (int) $this->ref,
+            'price' => (int) $this->price,
+            'offer_price' => (int) $this->offer_price,
+            'active' => (bool) $this->active,
+        ];
+       
+    }
+}

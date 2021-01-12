f4dcc40e2bb9160a889d429c3d0927e0d7deb3b6
hectorsc <makarron90@gmail.com>
2021-01-12 20:49:44 +0100

    ProductController:: arreglo para el formato del select

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
@@ -22,6 +21,7 @@ class ProductController extends Controller
 
     public function store(ProductRequest $request)
     {
+        $request->merge(['category_id' => $request->category_id['value']]);
         $request->user()->products()->create($request->all());
         return response()->json(['message' => 'Created successfully'], 200); 
     }
@@ -35,6 +35,7 @@ class ProductController extends Controller
    
     public function update(ProductRequest $request, Product $product)
     {
+        $request->merge(['category_id' => $request->category_id['value']]);
         $product->update($request->except('category'));
         return response()->json(['message' => 'Updated successfully'], 200);
     }

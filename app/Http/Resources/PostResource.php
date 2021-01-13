54778d10cd32aa9e5b5e93ef2887fe809f9e04d7
hectorsc <makarron90@gmail.com>
2021-01-13 18:02:48 +0100

    Resources:: Añadido PostResoure y collection

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\MyLibrary;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->when(MyLibrary::routeHaveMiddlewareAuth($request), (int) $this->id),
+            'title' => (string) $this->title,
+            'sub_title' => (string) $this->sub_title,
+            'body' => (string) $this->body,
+            'active' => $this->when(MyLibrary::routeHaveMiddlewareAuth($request), (bool) $this->active),
+            'tags' => TagResource::collection($this->whenLoaded('tags'))
+        ];
+    }
+}

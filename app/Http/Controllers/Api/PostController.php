dbd40fc4848527faef36b7bbd6112e9ff779abae
hectorsc <makarron90@gmail.com>
2021-01-13 13:28:57 +0100

    PostController añadido

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Post $post)
+    {
+        //
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+    
+    public function destroy(Post $post)
+    {
+        //
+    }
+}

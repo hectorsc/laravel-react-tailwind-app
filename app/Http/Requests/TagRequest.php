6aff441ee7985a8e2ee7e0a17f09c66ba081dca6
hectorsc <makarron90@gmail.com>
2021-01-13 18:00:28 +0100

    Request:: creado TagRequest

---
+++ app/Http/Requests/TagRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+
+class TagRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(Request $request)
+    {
+        $rules = [
+            'name' => [
+                'required', 'min:4', 'max:20',
+                Rule::unique('tags')->where(function ($query) use ($request) {
+                    return $query->where('user_id', $request->user()->id)->where('name', $request->name);
+                })
+            ]
+        ];
+
+        if ($this->tag) {
+            if ($this->tag->name == $request->name) {
+                $rules = ['name' => 'required|min:4|max:20'];
+            }
+        }
+
+        return $rules;
+        
+    }
+}

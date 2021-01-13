217e2cc4a4795f6d37cc58bc7b46895359c37f78
hectorsc <makarron90@gmail.com>
2021-01-13 13:23:25 +0100

    BBDD:: Tag model, factory, seeder, migracion

---
+++ app/Models/Tag.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}

29564f792cee75af09530d883cb91dd5e6914b08
hectorsc <makarron90@gmail.com>
2021-01-13 19:37:55 +0100

    Request:: creado PostRequest

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'title' => [
+                'required', 'min:4', 'max:150',
+                Rule::unique('posts')->where(function ($query) use ($request) {
+                    return $query->where('user_id', $request->user()->id)->where('title', $request->title);
+                })
+            ],
+            'sub_title' => 'required|min:4|max:150',
+            'body' => 'required|min:10',
+            'tags' => 'required'
+        ];
+
+        if ($this->post) {
+            if ($this->post->title == $request->title) {
+                $rules = [
+                    'title' => 'required|min:4|max:150',
+                    'sub_title' => 'required|min:4|max:150',
+                    'body' => 'required|min:10',
+                    'tags' => 'required'
+                ];
+            }
+        }
+
+        return $rules;
+        
+    }
+}

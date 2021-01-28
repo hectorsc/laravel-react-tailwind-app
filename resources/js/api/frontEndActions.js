3342a125157e1fac8b3c21c01a31c8e01c4dffc2
hectorsc <makarron90@gmail.com>
2021-01-28 21:20:01 +0100

    React:: frontend action - endpoints -

--- resources/js/api/frontEndActions.js
+++ resources/js/api/frontEndActions.js
@@ -8,3 +8,30 @@ export const allCategories = async () => {
       return e.response.data;
    }
 };
+
+export const allPosts = async () => {
+   try {
+      const response = await axios.get('/api/getPosts');
+      return response.data;
+   } catch (e) {
+      return e.response.data;
+   }
+};
+
+export const allProducts = async () => {
+   try {
+      const response = await axios.get('/api/getProducts');
+      return response.data;
+   } catch (e) {
+      return e.response.data;
+   }
+};
+
+export const allTags = async () => {
+   try {
+      const response = await axios.get('/api/getTags');
+      return response.data;
+   } catch (e) {
+      return e.response.data;
+   }
+};

29ec56fb9503e215b9ce2d41439083d54daad429
hectorsc <makarron90@gmail.com>
2021-02-14 21:07:44 +0100

    Providers:: añadido welcome email despues de verified

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Listeners\SendEmailRegisteredUserNotification;
 use App\Listeners\SendEmailWelcomeUserNotification;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
@@ -21,6 +22,9 @@ class EventServiceProvider extends ServiceProvider
             SendEmailVerificationNotification::class,
             SendEmailRegisteredUserNotification::class,
             SendEmailWelcomeUserNotification::class,
+        ],
+        Verified::class => [
+            // SendEmailWelcomeUserNotification::class,
         ],
     ];
 
